cbbf8b17d9034a0c9cd85f11a962b6de411dd9f7
FlimFli <peckbboy2@gmail.com>
2025-11-21 10:12:43 +0700

    secon commit

--- bin/dart_week5.dart
+++ bin/dart_week5.dart
@@ -1,10 +1,14 @@
 import 'dart:io';
 
+void main(){
+  printName();
+  //add(10, 5);
+}
+
 void add(int num1,int num2){
   int sum = num1 + num2;
   print(sum);
 }
-
 
 void printName(){
   String fullName = 'Flim Fl';
@@ -13,9 +17,4 @@ void printName(){
   int num1 = int.parse(stdin.readLineSync()!);
   int num2 = int.parse(stdin.readLineSync()!);
   add(num1, num2);
-}
-
-void main(){
-  printName();
-  //add(10, 5);
 }
